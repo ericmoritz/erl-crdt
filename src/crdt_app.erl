3057a5b27bfa43e71ff4a5ad2a7a774c7cf1bb41
Eric Moritz <emoritz@gannett.com>
2012-04-26 17:12:41 -0400

    Added the initial build of the crdt app

---
+++ src/crdt_app.erl
@@ -0,0 +1,16 @@
+-module(crdt_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    crdt_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/twopset.erl
@@ -0,0 +1,47 @@
+-module(twopset).
+-export([new/0, add_element/2, del_element/2, is_element/2, merge/2]).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+new() ->
+    {sets:new(), sets:new()}.
+
+merge({A1, R1}, {A2, R2}) ->
+    A3 = sets:union(A1, A2),
+    R3 = sets:union(R1, R2),
+    {A3, R3}.
+
+is_element(Element, {ASet, RSet}) ->
+    sets:is_element(Element, ASet) and not sets:is_element(Element, RSet).
+
+add_element(Element, {ASet, RSet}) ->
+    {sets:add_element(Element, ASet), RSet}.
+
+del_element(Element, {ASet, RSet}) ->
+    {ASet, sets:add_element(Element, RSet)}.
+
+from_list(L) ->
+    lists:foldl(fun add_element/2, new(), L).
+
+-ifdef(TEST).
+
+merge_test() ->
+    S = from_list([eric, glenn, shawn]),
+    
+    % diverge from S
+    S1 = del_element(glenn, S),
+    S2 = del_element(shawn, add_element(shawn, S)),
+
+    S3 = merge(S1, S2),
+
+    ?assert(is_element(eric, S3)),
+    ?assert(not is_element(shawn, S3)),
+    ?assert(not is_element(glenn, S3)),
+    
+    % removed items from a twopset can not be added back
+    S4 = add_element(shawn, S3),
+    ?assert(not is_element(shawn, S4)).
+
+-endif.
